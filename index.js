7c86e57bb8756f56abe7f494f8efcf70534b1426
Marycarmen Zambrano <zambranomarycarmen48@gmail.com>
2022-09-07 03:10:29 +0000

    carta uno

---
+++ index.js
@@ -0,0 +1,22 @@
+let cards = ['♦', '♥', '♠ ', ' ♣'];
+let numbers = ['5', 'J', 'K', '3', '8', 'A', '10', '2', '7', '9', '4'];
+
+/*function displayCard() {
+    let singleCard = cards[Math.floor(Math.random() * cards.length)];
+    let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
+    let showCard = singleCard + "    " + singleNumber;
+    document.getElementById("resul").style.background ='#accff';
+    document.getElementById("cards").style.background ='#accff';
+    document.getElementById("numbers").style.background ='#accff';
+
+    document.getElementById("resul").innerHTML = showCard;
+    document.getElementById("cards").innerHTML = showCard;
+    document.getElementById("numbers").innerHTML = showCard;
+}*/
+
+
+
+
+
+
+
