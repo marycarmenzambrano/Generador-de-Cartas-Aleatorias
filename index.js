0aa8cae3d20ca9b56ba87ac7728bab034a73e5df
Marycarmen Zambrano <zambranomarycarmen48@gmail.com>
2022-09-07 22:52:24 +0000

    se modifica codigo

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 let cards = ['♦', '♥', '♠ ', ' ♣'];
 let numbers = ['5', 'J', 'K', '3', '8', 'A', '10', '2', '7', '9', '4'];
 
-/*function displayCard() {
+function displayCard() {
     let singleCard = cards[Math.floor(Math.random() * cards.length)];
     let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
-    let showCard = singleCard + "    " + singleNumber;
-    document.getElementById("resul").style.background ='#accff';
-    document.getElementById("cards").style.background ='#accff';
-    document.getElementById("numbers").style.background ='#accff';
+   
+    document.getElementById("pinta-arriba").style.background ='#accff';
+    document.getElementById("numeros").style.background ='#accff';
+    document.getElementById("pinta-abajo").style.background ='#accff';
 
-    document.getElementById("resul").innerHTML = showCard;
-    document.getElementById("cards").innerHTML = showCard;
-    document.getElementById("numbers").innerHTML = showCard;
-}*/
+    document.getElementById("pinta-arriba").innerHTML = singleCard;
+    document.getElementById("numeros").innerHTML = singleNumber;
+    document.getElementById("pinta-abajo").innerHTML = singleCard;
+}
 
 
 
